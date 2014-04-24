84baa9c24a292ba8e6b44cd954fcf4cab26f0e7d
Neil Patterson <neil.patterson@finalsite.com>
2014-04-23 23:24:08 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prettyquotes::Application.config.secret_key_base = '9546ffd826ec43a03fc2ece1748ae20b959c9328271db5d194d87cf2736ea4c1b262de38ca8d52ba42d7a6c5c24ba2f8c79a803c4fbc92ff8643d4a2ec4868a6'
